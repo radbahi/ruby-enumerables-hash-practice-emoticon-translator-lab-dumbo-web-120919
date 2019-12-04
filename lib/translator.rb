b02e5b49815141b459e1f16839847a00f5d9533a
Radouane Bahi <radbahi@gmail.com>
2019-12-04 19:19:40 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -1,31 +1,31 @@
 require 'yaml'
 
 def load_library(emoticonYAML)
-  emoticons = YAML.load_file('./lib/emoticons.yml')
+  emoticonLoad = YAML.load_file('./lib/emoticons.yml')
 
   emoticon_lib = {'get_meaning'  => {},
                   'get_emoticon' => {} }
 
-  emoticons.each do |meaning, value|  #
+  emoticons.each do |meaning, value|
    
     english = value[0]
     japanese = value[1]
     
-    emoticon_lib['get_meaning'][japanese] = meaning
-    emoticon_lib['get_emoticon'][english] = japanese
+    emoticonLoad['get_meaning'][japanese] = meaning
+    emoticonLoad['get_emoticon'][english] = japanese
   end
-  emoticon_lib
+  emoticonLoad
 end
 
 
-def get_japanese_emoticon(emoticon_file, emoticon)
-  emoticon_lib = load_library(emoticon_file)
-  japanese_emoticon = emoticon_lib['get_emoticon'][emoticon]
-  japanese_emoticon ? japanese_emoticon : 'Sorry, that emoticon was not found'
+def get_japanese_emoticon(emoticonYAML, emoticon)
+  emoticonExtract = load_library(emoticonYAML)
+  japaneseEmoticon = emoticonExtract['get_emoticon'][emoticon]
+  japaneseEmoticon ? japaneseEmoticon : 'Error, no emoticon found'
 end
 
-def get_english_meaning(emoticon_file, emoticon)
-  emoticon_lib = load_library(emoticon_file)
+def get_english_meaning(emoticonYAML, emoticon)
+  emoticon_lib = load_library(emoticonYAML)
   english_meaning = emoticon_lib['get_meaning'][emoticon]
   english_meaning ? english_meaning : 'Sorry, that emoticon was not found'
 end
