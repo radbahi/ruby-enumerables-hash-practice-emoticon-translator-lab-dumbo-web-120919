c001b4de81af4a528b9bfc5b0a839962f74e142b
Radouane Bahi <radbahi@gmail.com>
2019-12-04 19:16:40 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -1,14 +1,16 @@
 require 'yaml'
 
-def load_library(emoticon_file)
+def load_library(emoticonYAML)
   emoticons = YAML.load_file('./lib/emoticons.yml')
 
   emoticon_lib = {'get_meaning'  => {},
                   'get_emoticon' => {} }
 
-  emoticons.each do |meaning, value|  # meaning= "surprised"   # value= [":o", "o_O"]  <-(e,j)
+  emoticons.each do |meaning, value|  #
+   
     english = value[0]
     japanese = value[1]
+    
     emoticon_lib['get_meaning'][japanese] = meaning
     emoticon_lib['get_emoticon'][english] = japanese
   end
